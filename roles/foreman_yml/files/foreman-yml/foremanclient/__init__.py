e8ce2536fbc9554d1b72c41271b676e4fd7f8727
hamza <hamza@localhost.localdomain>
2017-04-27 15:51:56 +0200

    adding submodule files

---
+++ roles/foreman_yml/files/foreman-yml/foremanclient/__init__.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+
+import sys
+import logging
+import log
+from foremanclient.validator import Validator
+from foreman.client import Foreman, ForemanException
+from pprint import pprint
+
+
+class ForemanBase:
+
+    def __init__(self, config, loglevel=logging.INFO):
+        logging.basicConfig(level=loglevel)
+        self.config = config['foreman']
+        self.loglevel = loglevel
+        self.validator = Validator()
+
+
+    def get_config_section(self, section):
+        try:
+            cfg = self.config[section]
+        except:
+            cfg = []
+        return cfg
+
+
+    def connect(self):
+        try:
+            logging.disable(logging.WARNING)
+            self.fm = Foreman(self.config['auth']['url'], (self.config['auth']['user'], self.config['auth']['pass']), api_version=2, use_cache=False, strict_cache=False)
+            # this is nescesary for detecting faulty credentials in yaml
+            self.fm.architectures.index()
+            logging.disable(self.loglevel-1)
+        except:
+            log.log(log.LOG_ERROR, "Cannot connect to Foreman-API")
+            sys.exit(1)
+
+
+    def get_api_error_msg(self, e):
+        dr = e.res.json()
+        try:
+            msg = dr['error']['message']
+        except KeyError:
+            msg = dr['error']['full_messages'][0]
+
+        return msg
+
+
+    def get_host(self, host_id):
+        host = self.fm.hosts.show(id=host_id)
+        return host
+
+
+    def remove_host(self, host_id):
+        try:
+            self.fm.hosts.destroy(id=host_id)
+            return True
+        except:
+            return False
+
+
+    def dict_underscore(self, d):
+        new = {}
+        for k, v in d.iteritems():
+            if isinstance(v, dict):
+                v = self.dict_underscore(v)
+            new[k.replace('-', '_')] = v
+        return new
+
+    def dict_dash(self, d):
+        new = {}
+        for k, v in d.iteritems():
+            if isinstance(v, dict):
+                v = self.dict_dash(v)
+            new[k.replace('_', '-')] = v
+        return new
+
+    def filter_dump(self, dump, wanted_keys):
+        ret = {}
+        dd = self.dict_dash(dump)
+        for setting in dd:
+            if setting in wanted_keys:
+                value = dd[setting]
+                if value is None or value=='':
+                    continue
+                ret[setting] = value
+        return ret
+
+
+    def get_audit_ip(self, host_id):
+        # this is hacky right now since the audits-api has an bug and does not return any audits whe passing host_id
+        # host_audits = self.fm.hosts.audits_index(auditable_id=80)
+        #ha = self.fm.hosts.index(auditable_id=81)
+        host_audits = []
+        host_ip = False
+        all_audits = self.fm.audits.index(per_page=99999)['results']
+
+        # get audits of specified host
+        for audit in all_audits:
+            if ( audit['auditable_type'] == 'Host') and (audit['auditable_id'] == host_id):
+                host_audits.append(audit)
+
+        # search for audit type audited_changes['build']
+        for audit in host_audits:
+            if 'installed_at' in audit['audited_changes']:
+                try:
+                    ll = len(audit['audited_changes']['installed_at'])
+                    host_ip = audit['remote_address']
+                    return host_ip
+                except:
+                    pass
+
+        # nothing found, return False
+        return False
---
+++ roles/foreman_yml/files/foreman-yml/foremanclient/foreman_yml.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import yaml
+import sys
+from foremanclient.importer import ForemanImport
+from foremanclient.dump import ForemanDump
+from foremanclient.cleanup import ForemanCleanup
+import os
+
+import log
+
+
+def fm_dump(fm):
+    fm.dump()
+
+
+def fm_cleanup(fm):
+    # cleanup architecture
+    fm.process_cleanup_arch()
+
+    # cleanup medium
+    fm.process_cleanup_medium()
+
+    # cleanup partition table
+    fm.process_cleanup_ptable()
+
+    # cleanup provisioning template
+    fm.process_cleanup_provisioningtpl()
+
+
+def fm_import(fm):
+    # setting
+    fm.process_config_settings()
+
+    # architecture
+    fm.process_config_arch()
+
+    # smart proxy
+    fm.process_config_smartproxy()
+
+    # domain
+    fm.process_config_domain()
+
+    # subnet
+    fm.process_config_subnet()
+
+    # environment
+    fm.process_config_enviroment()
+
+    # model
+    fm.process_config_model()
+
+    # medium
+    fm.process_config_medium()
+
+    # partition table
+    fm.process_config_ptable()
+
+    # operating system
+    fm.process_config_os()
+
+    # provisioning template
+    fm.process_config_provisioningtpl()
+
+    # hostgroup
+    fm.process_config_hostgroup()
+
+    # Link items to operating system
+    fm.process_config_os_link()
+
+    # Link template-combination-attribute
+    fm.process_template_combination_attribute()
+
+    # host
+    fm.process_config_host()
+
+    # enterprise edition only: ldap
+    fm.process_auth_sources_ldap()
+
+    # users
+    fm.process_config_user()
+
+    # user groups
+    fm.process_usergroups()
+
+    # roles
+    fm.process_roles()
+
+
+
+
+def main():
+    global LOGLEVEL
+
+    try:
+        function = sys.argv[1]
+    except:
+        log.log(log.LOG_ERROR, "No action defined (Valid: dump, import, cleanup)")
+        sys.exit(1)
+
+
+    if os.path.isfile(sys.argv[1]):
+        config_file = sys.argv[1]
+        function = "legacy"
+    else:
+        try:
+            config_file = sys.argv[2]
+        except IndexError:
+            log.log(log.LOG_ERROR, "No YAML provided")
+            sys.exit(1)
+
+    try:
+        config_file = open(config_file, 'r')
+        config = yaml.load(config_file)
+        config_file.close()
+    except:
+        log.log(log.LOG_ERROR, "Failed to load/parse config")
+        sys.exit(1)
+
+
+    if (function=="import"):
+        fm = ForemanImport(config)
+        fm.connect()
+        fm_import(fm)
+
+    if (function=="dump"):
+        fm = ForemanDump(config)
+        fm.connect()
+        fm_dump(fm)
+
+    if (function=="cleanup"):
+        fm = ForemanCleanup(config)
+        fm_cleanup(fm);
+
+    if (function=="legacy"):
+        fm_cls = ForemanCleanup(config)
+        fm_cls.connect()
+        fm_cleanup(fm_cls)
+        fm_imp = ForemanImport(config)
+        fm_imp.connect()
+        fm_import(fm_imp)
+
+
+
+
+if __name__ == '__main__':
+    main()
